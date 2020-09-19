c175fe2da626a80a2ba0ff9377bb7bae9b2965c6
maliex <malikopeyemi5@gmail.com>
2020-09-19 09:14:08 +0100

    My changes

--- index.js
+++ index.js
@@ -16,9 +16,44 @@ new Autocomplete(document.getElementById('state'), {
 });
 
 
-// Github Users
-// new Autocomplete(document.getElementById('gh-user'), {
-//   onSelect: (ghUserId) => {
-//     console.log('selected github user id:', ghUserId);
-//   },
-// });
+document.getElementById('gh-user').addEventListener("input", function (e){
+	var search_text = e.target.value;
+
+	function autocomplete(_data){
+		const _elem = document.getElementById('gh-user-result');
+		_elem.style.display = 'block';
+		for (var i = 0; i < _data.length; i++) {
+			var node = document.createElement("div");
+			node.setAttribute("id", _data[i].login);
+			node.innerHTML = _data[i].login;
+			node.onclick = function () {
+				console.log(_data[i].login);
+			};
+			_elem.appendChild(node);
+		}
+	}
+
+	const url = 'https://api.github.com/search/users?q='+search_text+'&per_page=60';
+
+	fetch(url)  
+	  .then(  
+	    function(response) {  
+	      if (response.status !== 200) {  
+	        console.warn('Looks like there was a problem. Status Code: ' + 
+	          response.status);  
+	        return;  
+	      }
+
+	      // Examine the text in the response  
+	      response.json().then(function(data) {  
+	    	const total_count = data['total_count'];
+	    	const auto_data = data['items'];
+	    	console.log(auto_data);
+	    	autocomplete(auto_data);
+	      });  
+	    }  
+	  )  
+	  .catch(function(err) {  
+	    console.error('Fetch Error -', err);  
+	});
+});
